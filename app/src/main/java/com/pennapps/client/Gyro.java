8a1796e1b0af3c4b0c45dc26ce28c9696a958e53
Sail338 <dude.ex001@gmail.com>
2016-01-23 04:02:57 -0500

    fuck code

---
+++ app/src/main/java/com/pennapps/client/Gyro.java
@@ -0,0 +1,10 @@
+package com.pennapps.client;
+
+import android.hardware.SensorEventListener;
+
+/**
+ * Created by hari on 1/23/16.
+ */
+public abstract class Gyro implements SensorEventListener {
+
+}
--- app/src/main/java/com/pennapps/client/MainActivity.java
+++ app/src/main/java/com/pennapps/client/MainActivity.java
@@ -1,9 +1,8 @@
 package com.pennapps.client;
 
+import android.app.Activity;
 import android.content.Context;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorManager;
+import android.hardware.*;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,17 +16,23 @@ import io.socket.client.Socket;
 import java.net.URISyntaxException;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     private SensorManager manager;
     private Sensor sensor;
-    float[] coords;
+    float[] coords = new float[3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        manager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        sensor = manager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        try {
+            manager = (SensorManager) getApplicationContext().getSystemService(Context.SENSOR_SERVICE);
+            sensor = manager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+        } catch (Exception e){
+            Log.e("ERROR",e.getMessage());
+        }
+        manager.registerListener(listener,sensor,manager.SENSOR_DELAY_FASTEST);
         socket.connect();
 
     }
@@ -53,19 +58,22 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-   private Socket socket;
+
+    private Socket socket;
+
     {
-        try{
+        try {
 
             socket = IO.socket("http://10.103.226.100:420");
             socket.open();
-        //creates sensor manager
-            for(int i=0;i<1000;i++) {
-                socket.emit("satan",sensor.getName() );
+            //creates sensor manager
+            for (int i = 0; i < 1000; i++) {
+
+
 
             }
-        } catch(URISyntaxException e){
-            Log.e("ERROR",e.getMessage());
+        } catch (URISyntaxException e) {
+            Log.e("ERROR", e.getMessage());
         }
 
     }
@@ -74,50 +82,67 @@ public class MainActivity extends AppCompatActivity {
     private final float[] deltaRotationVector = new float[4];
     private float timestamp;
 
-    public  void onSensorChanged(SensorEvent event) {
-        // This timestep's delta rotation to be multiplied by the current rotation
-        // after computing it from the gyro sample data.
-        Log.e("TIMESTAMPE",Float.toString(timestamp));
-        if (timestamp != 0) {
-            final float dT = (event.timestamp - timestamp) * NS2S;
-            // Axis of the rotation sample, not normalized yet.
-            float axisX = event.values[0];
-            float axisY = event.values[1];
-            float axisZ = event.values[2];
+    SensorEventListener listener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            Log.i("SHIT",event.sensor.getName());
+            Log.i("DIp",Float.toString(event.values[0]));
+            //coords[0] = event.values[0];
+            // This timestep's delta rotation to be multiplied by the current rotation
+            // after computing it from the gyro sample data.
+           // Log.e("TIMESTAMPE", Float.toString(timestamp));
+            if (timestamp != 0) {
+               final float dT = (event.timestamp - timestamp) * NS2S;
+                // Axis of the rotation sample, not normalized yet.
+                float axisX = event.values[0];
+                float axisY = event.values[1];
+                float axisZ = event.values[2];
 
-            // Calculate the angular speed of the sample
-            double omegaMagnitude = Math.sqrt(axisX*axisX + axisY*axisY + axisZ*axisZ);
 
-            // Normalize the rotation vector if it's big enough to get the axis
-            // (that is, EPSILON should represent your maximum allowable margin of error)
-            if (omegaMagnitude > 1) {
-                axisX /= omegaMagnitude;
-                axisY /= omegaMagnitude;
-                axisZ /= omegaMagnitude;
+
+                // Calculate the angular speed of the sample
+                double omegaMagnitude = Math.sqrt(axisX * axisX + axisY * axisY + axisZ * axisZ);
+
+                // Normalize the rotation vector if it's big enough to get the axis
+                // (that is, EPSILON should represent your maximum allowable margin of error)
+                if (omegaMagnitude > 1) {
+                    axisX /= omegaMagnitude;
+                    axisY /= omegaMagnitude;
+                    axisZ /= omegaMagnitude;
+                }
+
+                // Integrate around this axis with the angular speed by the timestep
+                // in order to get a delta rotation from this sample over the timestep
+                // We will convert this axis-angle representation of the delta rotation
+                // into a quaternion before turning it into the rotation matrix.
+                float thetaOverTwo = (float) omegaMagnitude * dT / 2.0f;
+                float sinThetaOverTwo = (float) Math.sin(thetaOverTwo);
+                float cosThetaOverTwo = (float) Math.cos(thetaOverTwo);
+                deltaRotationVector[0] = sinThetaOverTwo * axisX;
+                deltaRotationVector[1] = sinThetaOverTwo * axisY;
+                deltaRotationVector[2] = sinThetaOverTwo * axisZ;
+                deltaRotationVector[3] = cosThetaOverTwo;
             }
+            timestamp = event.timestamp;
+            float[] deltaRotationMatrix = new float[9];
+            manager.getRotationMatrixFromVector(deltaRotationMatrix, deltaRotationVector);
 
-            // Integrate around this axis with the angular speed by the timestep
-            // in order to get a delta rotation from this sample over the timestep
-            // We will convert this axis-angle representation of the delta rotation
-            // into a quaternion before turning it into the rotation matrix.
-            float thetaOverTwo = (float)omegaMagnitude * dT / 2.0f;
-            float sinThetaOverTwo = (float)Math.sin(thetaOverTwo);
-            float cosThetaOverTwo = (float)Math.cos(thetaOverTwo);
-            deltaRotationVector[0] = sinThetaOverTwo * axisX;
-            deltaRotationVector[1] = sinThetaOverTwo * axisY;
-            deltaRotationVector[2] = sinThetaOverTwo * axisZ;
-            deltaRotationVector[3] = cosThetaOverTwo;
+            coords[0] = deltaRotationVector[0];
+            Log.i("X", Float.toString(coords[0]));
+            coords[1] = deltaRotationVector[1];
+            coords[2] = deltaRotationVector[2];
+            socket.emit("satan", "{"+coords[0]+","+coords[1]+","+coords[2]+"}");
+            // User code should concatenate the delta rotation we computed with the current rotation
+            // in order to get the updated rotation.
+            // rotationCurrent = rotationCurrent * deltaRotationMatrix;
+   }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+
         }
-        timestamp = event.timestamp;
-        float[] deltaRotationMatrix = new float[9];
-        manager.getRotationMatrixFromVector(deltaRotationMatrix, deltaRotationVector);
-        Log.i("X",Float.toString(coords[0]));
-        coords[0] = deltaRotationVector[0];
-        coords[1] = deltaRotationVector[1];
-        coords[2] = deltaRotationVector[2];
-        // User code should concatenate the delta rotation we computed with the current rotation
-        // in order to get the updated rotation.
-        // rotationCurrent = rotationCurrent * deltaRotationMatrix;
-    }
-}
 
+
+    };
+
+}
